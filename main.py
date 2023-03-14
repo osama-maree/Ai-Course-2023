2320475f5bed9f917e079a11a62cf507e3ed8a0a
osama maree <osamamarei2001@gmail.com>
2023-03-14 19:02:09 +0200

    add another way-rec

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-def dfs(root,goal,graph,visited):
+def dfs(root,goal,graph,visited,path):
+    if root in visited:
+        return
     visited.append(root)
     if root == goal:
-        return  [root]
+        path.append(goal)
+        return path
     for i in graph.get(root,[]):
-        if i not in visited:
-            path = dfs(i,goal,graph,visited)
-            if path:
-                return [root] + path
+            if dfs(i,goal,graph,visited,path):
+                path.insert(0,root)
+                return path
 
-    return  []
+    return path
 
 
 graph ={1:[2,3] ,2:[5,6],3:[7,8],5:[9],6:[2,5],7:[2,4],8:[1]} #direction graph
-goal = 5
-visited=[]
-print(dfs(list(graph.keys())[0],goal,graph,visited))
+goal = 4
+visited, path = [], []
+print(dfs(list(graph.keys())[0],goal,graph,visited,path))
--- recursionDFS.py
+++ recursionDFS.py
@@ -12,6 +12,28 @@ def dfs(root,goal,graph,visited):
 
 
 graph ={1:[2,3] ,2:[5,6],3:[7,8],5:[9],6:[2,5],7:[2,4],8:[1]} #direction graph
-goal = 5
+goal = 9
 visited=[]
 print(dfs(list(graph.keys())[0],goal,graph,visited))
+# another solution using recursion
+"""
+def dfs(root,goal,graph,visited,path):
+    if root in visited:
+        return
+    visited.append(root)
+    if root == goal:
+        path.append(goal)
+        return path
+    for i in graph.get(root,[]):
+            if dfs(i,goal,graph,visited,path):
+                path.insert(0,root)
+                return path
+
+    return path
+
+
+graph ={1:[2,3] ,2:[5,6],3:[7,8],5:[9],6:[2,5],7:[2,4],8:[1]} #direction graph
+goal = 4
+visited, path = [], []
+print(dfs(list(graph.keys())[0],goal,graph,visited,path))
+"""
