776a2972e11d2b34d3652f26c41b3119543f6fa1
osama maree <osamamarei2001@gmail.com>
2023-03-16 00:33:21 +0200

    UCS

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
-def dfs(graph,goal,limit):
-    root = list(graph.keys())[0]
-    stack, temp, path, visited = [(root,1)], [], [], []
-    parent = {root: None}
-    while stack:
-        v,d = stack.pop()
-        if v == goal:
-            while parent[goal]:
-                path.insert(0, goal)
-                goal = parent[goal]
-            path.insert(0, root)
-            break
-        visited.append(v)
-        for i in graph.get(v,[]):
-            if i not in visited and d < limit:
-                 temp.append(i)
-                 parent[i] = v
-        while temp:
-            stack.append((temp.pop(),d+1))
-    return path,visited
+import heapq
+def uniform_cost_search(start, goal, graph):
+    visited = set()
+    heap = [(0, start, [])]
+    while heap:
+        (cost, node, path) = heapq.heappop(heap)
+        print(path)
+        if node not in visited:
+            visited.add(node)
+            path = path + [node]
+            if node == goal:
+                return (cost, path)
 
-print("*"*80,"\n\t\t\t\t\t\t\tOsama Maree\n","*"*80)
-graph ={1:[2,3] ,2:[5,6],3:[7,8],5:[9],6:[2,5],7:[2,4],8:[10]} #direction graph
-goal,maxD = 2,5
+            for i in graph[node]:
+                if i not in visited:
+                    total_cost = cost + graph[node][i]
+                    heapq.heappush(heap, (total_cost, i, path))
 
-for i in range(1,maxD):
-    path,visited = dfs(graph,goal,i)
-    if path:
-        print("found goal in level :",i,"and path :",path,"and visited node :",visited,sep=" ",end="*^^*")
-        break
-else:
-    print("goal not found")
+    return None
+hgraph = {'A': {'B':2, 'C':3},
+     'B': {'C':2, 'D':5},
+     'C': {'D':4, 'G':2},
+     'D': {'C':6, 'E':4},
+     'E': {'F':1},
+     'F': [],
+     'G': []}
+print(uniform_cost_search('A','F', hgraph))
+
+# print("*"*80,"\n\t\t\t\t\t\t\tOsama Maree\n","*"*80)
+# graph ={1:[2,3] ,2:[5,6],3:[7,8],5:[9],6:[2,5],7:[2,4],8:[10]} #direction graph
+# goal,maxD = 2,5
+#
+# for i in range(1,maxD):
+#     path,visited = dfs(graph,goal,i)
+#     if path:
+#         print("found goal in level :",i,"and path :",path,"and visited node :",visited,sep=" ",end="*^^*")
+#         break
+# else:
+#     print("goal not found")
 
 
 
