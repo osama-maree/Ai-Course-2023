2190116778e909bf779cd613f9129e7f7851d3fa
osama maree <osamamarei2001@gmail.com>
2023-03-16 12:19:00 +0200

    updated uniform search

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import heapq
 def uniform_cost_search(start, goal, graph):
-    visited =[]
+    visited = []
     priorityQueue = [(0, start, [])]
     while priorityQueue:
         (cost, node, path) = heapq.heappop(priorityQueue)#return path has smallest cost
         if node not in visited:
             visited.append(node)
-            path=path + [node]    #path.append(node) what is deferent ?????
+            path.append(node)    #path.append(node) what is deferent about path=path+[node] ?????
             if node == goal:
                 return (cost, path)
             for i in graph.get(node,[]):
                 if i not in visited:
                     total_cost = cost + graph[node][i]
-                    priorityQueue.append((total_cost, i, path))
+                    heapq.heappush(priorityQueue,(total_cost, i, path))
 
     return (False,False)
 
@@ -25,7 +25,7 @@ graph = {'A': {'B':2, 'C':5},
      'F':{'K':3,'L':4}
 
   }
-cost,path = uniform_cost_search('A','F', graph)
+cost,path = uniform_cost_search('A','G', graph)
 if cost:
     print(("the cost is :",cost," and the path is :",path))
 else:
